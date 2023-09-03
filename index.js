9bc38837813a4fff008d06d777c683bad2897068
steven dulworth <crowscoupetech@proton.me>
2023-09-03 15:27:27 -0500

    continued functions

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(){
+    return drivers.slice(0, 2);
+}
+
+const returnLastTwoDrivers = function(){
+    return drivers.slice(2, 5);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fare){
+    return function(){
+        return fare * 5; 
+    }
+}
+
+function fareDoubler(fare){
+    return fare * 2;
+}
+
+function fareTripler(fare){
+    return fare * 3;
+}
+
+function selectDifferentDrivers(drivers, list ){
+    return list(drivers);
+}
